3b9cbd3bb6f3bbadfbfb7f6898fee818607ed3d3
Denis <ivanenko@ucu.edu.ua>
2019-11-03 17:46:16 +0200

    Something

--- src/main/java/ua/edu/ucu/collections/immutable/Node.java
+++ src/main/java/ua/edu/ucu/collections/immutable/Node.java
@@ -1,16 +1,16 @@
 package ua.edu.ucu.collections.immutable;
 
-public final class Node  {
-    public Object value;
-    public Node next;
-    public  Node previous;
+final class Node  {
+    Object value;
+    Node next;
+    Node previous;
 
 
-    public Node(Object value) {
+    Node(Object value) {
         this.value = value;
     }
 
-    public Node(Node node) {
+    Node(Node node) {
         value = node.value;
         next = node.next;
         previous = node.previous;
